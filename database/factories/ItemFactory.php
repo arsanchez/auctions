db5c04849ab23fb46ead5fdc4cbdacacd333f83b
Argenis Sánchez <argenisphp@gmail.com>
2021-09-07 00:29:58 -0400

    Added the items and bids migrations

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Item;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -17,12 +17,12 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Item::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'description' => $faker->sentence(5),
+        'price' => $faker->numberBetween($min = 100, $max = 10000),
+        'photo_url' => $faker->imageUrl($width = 200, $height = 200),
+        'ends_at' => $faker->dateTimeBetween('now', '+15 days'),
     ];
 });
--- database/seeds/ItemSeeder.php
+++ database/seeds/ItemSeeder.php
@@ -1,16 +1,17 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Item;
 
-class DatabaseSeeder extends Seeder
+class ItemSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ItemSeeder::class);
+        factory(App\Item::class, 50)->create();
     }
 }
